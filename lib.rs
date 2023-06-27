6f6974f4f7fafa7c81b6b7ede5fa78212bb87f67
MaxPower <131723257+MaxPower0345@users.noreply.github.com>
2023-06-27 01:12:46 -0300

    Create structs full

--- lib.rs
+++ lib.rs
@@ -2,45 +2,78 @@
 
 #[ink::contract]
 mod Rust_Tp_Final {
-
-    /// Defines the storage of your contract.
-    /// Add new fields to the below struct in order
-    /// to add new static storage fields to your contract.
+    use ink::storage::Mapping;
+    use ink::prelude::vec::Vec;
+    use ink::prelude::string::String;
+    // no es mejor usar #[ink::storage_item] ¿? segun el ink implementa todos los traits
+    #[derive(scale::Decode, scale::Encode,Debug)]
+    #[cfg_attr(feature = "std",derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout))]
+    pub struct Fecha{ // se debe crear con lo que nos conto el profe
+        año:u32,
+        mes:u8,
+        dia:u16
+    }
+    #[derive(scale::Decode, scale::Encode,Debug)]
+    #[cfg_attr(feature = "std",derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout))]
+    pub enum CategoriasDisponibles{
+        A,
+        B{id_deporte:u32},
+        C
+    }
+    #[derive(scale::Decode, scale::Encode,Debug)]
+    #[cfg_attr(feature = "std",derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout))]
+    pub enum Actividades{
+        Futbol,
+        Gimnasio,
+        Basquet,
+        Rugby,
+        Hockey,
+        Natacion,
+        Tenis,
+        Paddle
+    }
+    #[derive(scale::Decode, scale::Encode,Debug)]
+    #[cfg_attr(feature = "std",derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout))]
+    pub struct DatosPersonalesSocio{
+        nombre:String,
+        apellidos:String
+    }
+    #[derive(scale::Decode, scale::Encode,Debug)]
+    #[cfg_attr(feature = "std",derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout))]
+    pub struct Categoria{
+        id:u32,
+        nombre:String,
+        costo_mensual_en_tokens:u32,
+        //id_de_actividades_accesibles: no encuentro un hashset
+    }
+    #[derive(scale::Decode, scale::Encode,Debug)]
+    #[cfg_attr(feature = "std",derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout))]
+    pub struct Socio{
+        id:u32,
+        categoria_id:u32,
+        datos_personales:DatosPersonalesSocio
+    }
+    #[derive(scale::Decode, scale::Encode,Debug)]
+    #[cfg_attr(feature = "std",derive(scale_info::TypeInfo, ink::storage::traits::StorageLayout))]
+    pub struct Pago{
+        id:u32,
+        fecha_de_pago:Option<Fecha>,
+        fecha_de_vencimiento:Fecha,
+        monto:u128,//   no deja utilizar f32 o f64 y no se por que?
+        pago_con_bonificacion:bool
+    }
     #[ink(storage)]
-    pub struct RustTpFinal {
-        /// Stores a single `bool` value on the storage.
-        value: bool,
+    pub struct ClubSemRust {
+        usuarios:Mapping<u32,Socio>,
+        actividades:Mapping<u32,Actividades>,
+        categorias:Mapping<u32,Categoria>,
+        cant_pagos_consecutivos_sin_atrasos_necesarios_paga_descuento:u32,
+        pagos:Vec<Pagos>
     }
 
-    impl RustTpFinal {
-        /// Constructor that initializes the `bool` value to the given `init_value`.
-        #[ink(constructor)]
-        pub fn new(init_value: bool) -> Self {
-            Self { value: init_value }
-        }
-
-        /// Constructor that initializes the `bool` value to `false`.
-        ///
-        /// Constructors can delegate to other constructors.
-        #[ink(constructor)]
-        pub fn default() -> Self {
-            Self::new(Default::default())
-        }
-
-        /// A message that can be called on instantiated contracts.
-        /// This one flips the value of the stored `bool` from `true`
-        /// to `false` and vice versa.
-        #[ink(message)]
-        pub fn flip(&mut self) {
-            self.value = !self.value;
-        }
-
-        /// Simply returns the current value of our `bool`.
-        #[ink(message)]
-        pub fn get(&self) -> bool {
-            self.value
-        }
+    impl ClubSemRust {
     }
+
 
     /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
     /// module and test functions are marked with a `#[test]` attribute.
@@ -50,93 +83,13 @@ mod Rust_Tp_Final {
         /// Imports all the definitions from the outer scope so we can use them here.
         use super::*;
 
-        /// We test if the default constructor does its job.
-        #[ink::test]
-        fn default_works() {
-            let Rust_Tp_Final = RustTpFinal::default();
-            assert_eq!(Rust_Tp_Final.get(), false);
-        }
-
         /// We test a simple use case of our contract.
         #[ink::test]
         fn it_works() {
-            let mut Rust_Tp_Final = RustTpFinal::new(false);
-            assert_eq!(Rust_Tp_Final.get(), false);
-            Rust_Tp_Final.flip();
-            assert_eq!(Rust_Tp_Final.get(), true);
-        }
-    }
-
-
-    /// This is how you'd write end-to-end (E2E) or integration tests for ink! contracts.
-    ///
-    /// When running these you need to make sure that you:
-    /// - Compile the tests with the `e2e-tests` feature flag enabled (`--features e2e-tests`)
-    /// - Are running a Substrate node which contains `pallet-contracts` in the background
-    #[cfg(all(test, feature = "e2e-tests"))]
-    mod e2e_tests {
-        /// Imports all the definitions from the outer scope so we can use them here.
-        use super::*;
-
-        /// A helper function used for calling contract messages.
-        use ink_e2e::build_message;
-
-        /// The End-to-End test `Result` type.
-        type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
-
-        /// We test that we can upload and instantiate the contract using its default constructor.
-        #[ink_e2e::test]
-        async fn default_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            // Given
-            let constructor = RustTpFinalRef::default();
-
-            // When
-            let contract_account_id = client
-                .instantiate("Rust_Tp_Final", &ink_e2e::alice(), constructor, 0, None)
-                .await
-                .expect("instantiate failed")
-                .account_id;
-
-            // Then
-            let get = build_message::<RustTpFinalRef>(contract_account_id.clone())
-                .call(|Rust_Tp_Final| Rust_Tp_Final.get());
-            let get_result = client.call_dry_run(&ink_e2e::alice(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), false));
-
-            Ok(())
-        }
-
-        /// We test that we can read and write a value from the on-chain contract contract.
-        #[ink_e2e::test]
-        async fn it_works(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
-            // Given
-            let constructor = RustTpFinalRef::new(false);
-            let contract_account_id = client
-                .instantiate("Rust_Tp_Final", &ink_e2e::bob(), constructor, 0, None)
-                .await
-                .expect("instantiate failed")
-                .account_id;
-
-            let get = build_message::<RustTpFinalRef>(contract_account_id.clone())
-                .call(|Rust_Tp_Final| Rust_Tp_Final.get());
-            let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), false));
-
-            // When
-            let flip = build_message::<RustTpFinalRef>(contract_account_id.clone())
-                .call(|Rust_Tp_Final| Rust_Tp_Final.flip());
-            let _flip_result = client
-                .call(&ink_e2e::bob(), flip, 0, None)
-                .await
-                .expect("flip failed");
-
-            // Then
-            let get = build_message::<RustTpFinalRef>(contract_account_id.clone())
-                .call(|Rust_Tp_Final| Rust_Tp_Final.get());
-            let get_result = client.call_dry_run(&ink_e2e::bob(), &get, 0, None).await;
-            assert!(matches!(get_result.return_value(), true));
-
-            Ok(())
+            let mut ClubSemRust = ClubSemRust::new(false);
+            assert_eq!(ClubSemRust.get(), false);
+            ClubSemRust.flip();
+            assert_eq!(ClubSemRust.get(), true);
         }
     }
 }
