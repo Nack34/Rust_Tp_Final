6c9b272245593e98e80b4a08bb97dcc84edad235
Nack34 <gmnack9@gmail.com>
2023-07-14 17:25:55 -0300

    Revert "arreglo_timestamp_con_milisegundos"
    This reverts commit 062bec2e74e219077a861fb8e2e0a3adc2607c7a.

--- reportes_club_sem_rust/target/ink/release/metadata-gen.d
+++ reportes_club_sem_rust/target/ink/release/metadata-gen.d
@@ -1,5 +1,1 @@
-<<<<<<< Updated upstream
 C:\Users\NICOLAS\Desktop\2023\Primer\ semestre\Rust\Rust_Tp_Final\reportes_club_sem_rust\target\ink\release\metadata-gen.exe: \\?\C:\Users\NICOLAS\AppData\Local\Temp\cargo-contract_v41TNR\.ink\metadata_gen\main.rs \\?\C:\Users\NICOLAS\Desktop\2023\Primer\ semestre\Rust\Rust_Tp_Final\reportes_club_sem_rust\lib.rs C:\Users\NICOLAS\Desktop\2023\Primer\ semestre\Rust\Rust_Tp_Final\reportes_club_sem_rust\registro_de_pagos_club_sem_rust\lib.rs
-=======
-C:\Users\fabri\OneDrive\Documentos\GitHub\Rust_Tp_Final\reportes_club_sem_rust\target\ink\release\metadata-gen.exe: \\?\C:\Users\fabri\AppData\Local\Temp\cargo-contract_byz34t\.ink\metadata_gen\main.rs \\?\C:\Users\fabri\OneDrive\Documentos\GitHub\Rust_Tp_Final\reportes_club_sem_rust\lib.rs C:\Users\fabri\OneDrive\Documentos\GitHub\Rust_Tp_Final\reportes_club_sem_rust\registro_de_pagos_club_sem_rust\lib.rs
->>>>>>> Stashed changes
